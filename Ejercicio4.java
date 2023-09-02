fe9b653f4d9cd1ad5de9d2b95a23e94627058149
NomadRoboto <142361258+NomadRoboto@users.noreply.github.com>
2023-09-02 16:43:13 -0500

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+     Scanner teclado = new Scanner(System.in);
+
+
+        int n;
+        int z;
+
+        System.out.print("Por favor ingrese el primer número a evaluar: ");
+        n = teclado.nextInt();
+        System.out.print("Por favor ingrese el segundo número a evaluar: ");
+        z=teclado.nextInt();
+
+        if(n>0 && z>0) 
+            System.out.println( "VERDADERO");
+        else if (n<0 && z<0)
+            System.out.println("verdadero");
+       else 
+            System.out.println("");
+
+
+
+}
+
+    }
