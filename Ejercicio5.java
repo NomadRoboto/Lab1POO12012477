68bd2b745057dc95c3404d80ce0134eb54c4f5f2
NomadRoboto <142361258+NomadRoboto@users.noreply.github.com>
2023-09-02 17:02:58 -0500

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+    System.out.print("Introduzca el numero a hallar el dia de la semana:");
+    int n = sc.nextInt();
+
+    switch (n){
+        case 1:System.out.println("Lunes"); break ;
+        case 2:System.out.println("Martes"); break ;
+        case 3:System.out.println("Miercoles"); break ;
+        case 4:System.out.println("Jueves"); break ;
+        case 5:System.out.println("Viernes"); break ;
+        case 6:System.out.println("Sabado"); break ;
+        case 7:System.out.println("Domingo"); break ;
+        default:System.out.println("NO EXISTE EL DIA"+n ); break ; 
+
+    }
+
+
+
+
+}
+
+    }
